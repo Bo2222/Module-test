57cf85805db2b69505ace7880ae5e8cbc1702a63
Bo Lin <learldu91@gmail.com>
2025-02-19 11:52:56 +0800

    定義好__init__檔案

---
+++ utilities/__init__.py
@@ -0,0 +1,8 @@
+#__init__.py
+
+#導入模組中的函數，使其在導入套件時可以直接使用
+from math_operations import add, subtract
+from string_operations import capitalize_string
+
+#可以進行套件初始化的設定
+package_name = "Utilities Package"
